6ea6c0be7f1a2bebf18949455f0fdf8b7e07e10d
Fadlalla Musa <fadlalla2000@gmail.com>
2021-07-27 16:34:00 +0000

    removed localhost

--- demo.py
+++ demo.py
@@ -101,4 +101,5 @@ def update_captions():
 
     
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
+#change to false since it the website may crash so it wont show any errors
